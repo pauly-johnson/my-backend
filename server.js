b3805d8d9febf3f395cb66c00bc964005081bbb6
pauly-johnson <paul.m.johnson1988@gmail.com>
2025-02-27 13:20:52 +1300

    h

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ app.use(express.json());
 
 // Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    
 })
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("MongoDB connection error:", err));
