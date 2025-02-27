cf240acf8c937c2e9bbf6ca814dad334148d717b
pauly-johnson <paul.m.johnson1988@gmail.com>
2025-02-27 14:23:28 +1300

    b

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,6 @@ router.post('/register', async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new User({ username, password: hashedPassword });
         await newUser.save();
-        res.json({ message: "Register route is working!" });
         res.status(201).json({ message: 'User registered successfully!' });
     } catch (err) {
         res.status(500).json({ error: err.message });
